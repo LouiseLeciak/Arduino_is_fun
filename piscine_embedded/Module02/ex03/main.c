6a9c85e71d31b73f90646eb4395139714299bdd0
lleciak <lleciak@f1br9s7.paris.42.school>
2026-04-16 16:11:12 +0200

    ex03 done

---
+++ piscine_embedded/Module02/ex03/main.c
@@ -0,0 +1,60 @@
+#include <avr/io.h>
+#include <util/delay.h>
+
+// screen /dev/ttyUSB0 115200
+// ctrl a puis k puis y
+
+#define BAUD 115200
+// p182 table 20-1
+#define MYUBRR ((F_CPU / (8 * BAUD)) - 1)
+
+// p184 20.5 USART initialization
+void uart_init() {
+    // set baudrate
+    // p180 table 20-1
+    UBRR0H = (unsigned char)(MYUBRR >> 8);
+    UBRR0L = (unsigned char) MYUBRR;
+    // enable transmiter
+    //p202 bit 4 Reveiver enable
+    UCSR0B = (1 << TXEN0) | (1 << RXEN0) | (1 << RXCIE0);
+    // 8n1 => 8 bits de data, no parite, 1 bit de stop
+    // p203 20-11
+    UCSR0C |= (1 << UCSZ01) | (1 << UCSZ00);
+
+    //p182 tab 20-1 for double speed to lower fail chance
+    UCSR0A |= (1 << U2X0);
+
+}
+
+void uart_tx(char c) {
+    // waiting for the buffer to be empty
+    while (!(UCSR0A & (1 << UDRE0))){}
+    // put data (char) into buffer
+    UDR0 = c;
+}
+
+char uart_rx(void) {
+    //p189, wait for data to be received
+    while (!(UCSR0A & (1 << RXC0))){}
+    // get and return received data from buffer
+    return UDR0;
+}
+
+char invert_case(char c){
+    if (c >= 'a' && c <= 'z')
+        c -= 32;
+    else if (c >= 'A' && c <= 'Z')
+        c += 32;
+    return (c);
+}
+
+void __attribute__((signal, used)) __vector_18(void) {
+    uart_tx(invert_case(uart_rx()));
+}
+
+
+int main (void) {
+    SREG =(1 << 7);
+    uart_init();
+    while (1){}
+}
