d71f8464605cc1736d5e1fd445b0700be8a604c5
lleciak <lleciak@f1br10s7.paris.42.school>
2026-04-18 18:33:24 +0200

    ex02 mod03

--- piscine_embedded/Module03/ex02/main.c
+++ piscine_embedded/Module03/ex02/main.c
@@ -7,7 +7,8 @@ int main(void) {
     uint8_t pos = 0;
     init_rgb();
     while (1){
-        
+        wheel(pos++);
+        _delay_ms(10);
     }
         
 }
--- piscine_embedded/Module03/ex02/rgb.c
+++ piscine_embedded/Module03/ex02/rgb.c
@@ -1,11 +1,31 @@
 #include "utils.h"
 
 void init_rgb(){
+    DDRD |= (1 << PD3) | (1 << PD5) | (1 << PD6);
 
+    //p113 TIMER0 fast PMW
+    //table 15-8 (mode 3)
+    TCCR0A |= (1 << WGM00) | (1 << WGM01);
+    // p113 table 15-4 & 15-7
+    TCCR0A |= (1 << COM0A1) | (1 << COM0B1);
+
+    // prescaler 8 p117 table 15-9
+    TCCR0B |= (1 << CS01);
+
+    // p141 TIMER1 for the last color
+    TCCR1A |= (1 << WGM10);
+    TCCR1A |= (1 << COM1A1);
+    TCCR1B |= (1 << WGM12);
+    TCCR1B |= (1 << CS11);
 }
 
 void set_rgb(uint8_t r, uint8_t g, uint8_t b){
-    
+    OCR0A = r;
+    OCR0B = g;
+    OCR1A = b;
+    // OCR0A = 255 - r;
+    // OCR0B = 255 - g;
+    // OCR1A = 255 - b;
 }
 
 void wheel(uint8_t pos) {
