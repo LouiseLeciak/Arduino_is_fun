801c8a5a6138fc64de924e28c2957768dd3cab36
lleciak <lleciak@f1br9s7.paris.42.school>
2026-04-23 13:41:41 +0200

    ex 00 mod05 done

--- piscine_embedded/Module05/ex00/main.c
+++ piscine_embedded/Module05/ex00/main.c
@@ -1,30 +1,48 @@
+
 #include "utils.h"
 
-int main(void) {
-    #include <avr/io.h>
-#include <util/delay.h>
+// https://www.f-legrand.fr/scidoc/docmml/sciphys/arduino/adcrapide/adcrapide.html
+// https://ece-classes.usc.edu/ee459/library/documents/ADC.pdf
+
+// 0x00 = 0V
+// 0xff = 5V
+// 0x80 ~= 2.5V
 
 void ADC_init() {
-    ADMUX = (1 << REFS0) | (1 << ADLAR); // AVCC + left adjust
-    ADCSRA = (1 << ADEN)                 // enable ADC
-           | (1 << ADPS2) | (1 << ADPS1) | (1 << ADPS0); // prescaler 128
+    // ADC multiplexer selection register
+    // REFS0 to choose AVCC
+    // ADLAR = i want to read 8 or 10 bits? p259 schema 24.9.3
+    // for 8 with ADLAR = 1 only have to read ADCH
+    ADMUX |= (1 << REFS0) | (1 << ADLAR);
+
+    // enable the adc
+    ADCSRA |= (1 << ADEN);
+    // prescaler,p249 max 200kHz, closer = more precise
+    // 16000000/64 = 250000 too much
+    // 16000000/128 = 125000
+    // p259 table 24-5
+    ADCSRA |= (1 << ADPS2) | (1 << ADPS1) | (1 << ADPS0);
 }
 
 uint8_t ADC_read() {
-    ADCSRA |= (1 << ADSC); // start conversion
-    while (ADCSRA & (1 << ADSC)); // wait
-    return ADCH; // 8 bits
+    //p252 ADC input Channels
+    // start a conversion
+    ADCSRA |= (1 << ADSC);
+    // wait until the conversion is over
+    while(ADCSRA & (1 << ADSC)){}
+
+    //p259 24.9.3.2
+    return ADCH;
 }
+
 
 int main() {
     ADC_init();
-
+    uart_init();
     while (1) {
-        uint8_t val = ADC_read();
-
-        // display with uart
-
+        uint8_t res = ADC_read();
+        uart_printhex(res);
+        uart_printstr("\n\r");
         _delay_ms(20);
     }
-}
 }
--- piscine_embedded/Module05/ex00/utils.h
+++ piscine_embedded/Module05/ex00/utils.h
@@ -3,13 +3,18 @@
 #define UTILS_H
 
 #include <avr/io.h>
+#include <util/delay.h>
 
+#define BAUD 115200
+// p182 table 20-1
+#define MYUBRR ((F_CPU / (8 * BAUD)) - 1)
 
 
 void uart_init();
 void uart_tx(char c);
 char uart_rx(void);
 void uart_printstr(const char* str);
+void uart_printhex(uint8_t value);
 
 
 #endif
