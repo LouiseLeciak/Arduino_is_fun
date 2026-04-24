5ba3fa208bc0522b25825e96fe518fde25a3d428
lleciak <lleciak@f1br9s7.paris.42.school>
2026-04-24 09:48:54 +0200

    ex02 decimal ok)

--- piscine_embedded/Module05/ex02/main.c
+++ piscine_embedded/Module05/ex02/main.c
@@ -1,19 +1,44 @@
 
 #include "utils.h"
 
-
 uint16_t ADC_read_10() {
-    //p252 ADC input Channels
-    // start a conversion
+    // p252 ADC input Channels
+    //  start a conversion
     ADCSRA |= (1 << ADSC);
     // ADLAR = i want to read 8 or 10 bits? p259 schema 24.9.3
     // for 8 with ADLAR = 1 only have to read ADCH
     // ADMUX |= (1 << ADLAR);
     // wait until the conversion is over
-    while(ADCSRA & (1 << ADSC)){}
+    while (ADCSRA & (1 << ADSC)) {
+    }
 
-    //p259 24.9.3.2
+    // p259 24.9.3.2
     return ADC;
+}
+
+void uart_printdec(uint16_t value) {
+    char buffer[5];
+    int i = 0;
+
+    // if 0
+    if (value == 0) {
+        uart_tx('0');
+        return;
+    }
+
+    // get numbers, reverse
+    while (value > 0) {
+        buffer[i++] = (value % 10) + '0';
+        value /= 10;
+    }
+
+    // print it in the right order
+    for (int j = i - 1; j >= 0; j--) {
+        char c[2];
+        c[0] = buffer[j];
+        c[1] = '\0';
+        uart_printstr(c);
+    }
 }
 
 
@@ -33,11 +58,11 @@ int main() {
         set_ADC(SET_NTC);
         res_n = ADC_read_10();
 
-        uart_printhex_10(res_r);
+        uart_printdec(res_r);
         uart_printstr(", ");
-        uart_printhex_10(res_l);
+        uart_printdec(res_l);
         uart_printstr(" ,");
-        uart_printhex_10(res_n);
+        uart_printdec(res_n);
         uart_printstr("\n\r");
         _delay_ms(20);
     }
