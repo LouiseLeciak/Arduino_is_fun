655f9575d124e99f96415beebfffc5899a2a4257
lleciak <lleciak@f1br10s7.paris.42.school>
2026-04-29 09:11:50 +0200

    ex00 mod08 done

--- piscine_embedded/Module08/ex00/main.c
+++ piscine_embedded/Module08/ex00/main.c
@@ -10,19 +10,37 @@ void SPI_master_init(void) {
     DDRB &= ~(1 << DDB4);
 
     // still p172
-    SPCR = (1 << SPE)// enable spi
-           | (1 << MSTR)//master mod
-           | (1 << SPR0);//clock rate
+    SPCR = (1 << SPE)      // enable spi
+           | (1 << MSTR)   // master mod
+           | (1 << SPR0);  // clock rate
 }
 
-void SPI_master_transmit(char c){
-    SPDR = c;
-    while(!(SPSR & (1 << SPIF))){};
+// transmit byte per byte
+void SPI_master_transmit(uint8_t data) {
+    SPDR = data;
+    while (!(SPSR & (1 << SPIF))) {
+    };
+}
+
+// https://docs.buspirate.com/docs/devices/apa102-sk9822/
+void choose_led_color(uint8_t r, uint8_t g, uint8_t b) {
+    // set 0 to clean
+    for (int i = 0; i < 4; i++) {
+        SPI_master_transmit(0x00);
+    }
+
+    // light the leds
+    SPI_master_transmit(0xFF);  // brigthness => first byte read
+    SPI_master_transmit(b);     // blue=> second byte read
+    SPI_master_transmit(g);
+    SPI_master_transmit(r);
+
 }
 
 int main() {
     SPI_master_init();
 
     while (1) {
+        choose_led_color(255, 0, 0);
     }
 }
